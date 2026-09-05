6d656f5ee857243c295d1c15f6145670ec9fe1af
Daniel275272 <daniel.barahona@campusucc.edu.co>
2026-09-05 01:48:54 +0000

    ejeplo de estructura de datos 1 version

---
+++ Semana1/Ejecutar.java
@@ -0,0 +1,31 @@
+package Semana1;
+
+public class Ejecutar {
+    public static void main(String[] args) {
+        int[] a = {5, 16, 8, 9, 10, 25, 19, 3, 6, 22, 2};
+        
+            for (int i = 0; i < a.length; i++) {
+        System.out.println("a[" + i + "]=" + a[i]);
+        }
+        int suma=0;
+        for (int i = 0; i < a.length; i++) {
+            suma+=a[i];
+        }
+        System.out.println("la suma de los elementos del arreglo a"+ suma);
+
+        int[] b = new int[11];
+        int min=1, max=50;
+        for (int i = 0; i < b.length; i++) {
+            b[i]=(int) (Math.random()* (max-min-1)+min);
+        }
+
+        for (int i = 0; i < b.length; i++){
+            System.out.println("b["+i+"]="+b[i]);
+        }
+
+        for(int i = 0 ;i < b.length;i++){
+            System.out.println("b["+i+"]="+b[i]);
+        }
+
+    }
+}
